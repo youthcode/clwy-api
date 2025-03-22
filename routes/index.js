f9afdf48dcdf6f92d368bb52878af06b7c1c71af
wangyupeng <wangyupeng@haiwainet.cn>
2025-03-22 18:25:57 +0800

    代码更新

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ message: 'Hello World 222' });
+router.get('/hello', function(req, res) {
+  res.json({ message: 'Hello World 666' });
 });
 
 module.exports = router;
