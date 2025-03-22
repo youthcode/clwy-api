9878cedf4d3349e9cc6639900551d3ffe74ca4b0
wangyupeng <wangyupeng@haiwainet.cn>
2025-03-22 20:46:55 +0800

    10-19

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.json({ message: 'Hello World 999' });
+  res.json({ message: 'Hello World 10-10' });
 });
 
 module.exports = router;
